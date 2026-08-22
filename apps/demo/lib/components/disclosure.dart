939c7c5878b7fc80070c7588dddde574dfc2bd06
Leo Farias <leoafarias@gmail.com>
2026-08-22 13:39:06 -0400

    fix(demo): import Widgetbook knob state helper

--- apps/demo/lib/components/disclosure.dart
+++ apps/demo/lib/components/disclosure.dart
@@ -1,4 +1,5 @@
 import 'package:demo/helpers/catalog.dart';
+import 'package:demo/helpers/use_case_state.dart';
 import 'package:flutter/material.dart';
 import 'package:remix/remix.dart';
 import 'package:remix_fortal/remix_fortal.dart';
